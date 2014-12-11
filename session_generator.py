568ea8c612fce6e0376829b860f7643fc8da7d34
PaulPidou <pidou.paul@gmail.com>
2014-12-11 21:01:19 +0100

    Update

--- session_generator.py
+++ session_generator.py
@@ -7,116 +7,206 @@ import base64, hashlib
 class Application():
     "Basic session generator"
     def __init__(self):
-        self.time, self.duration,self.encoding, self.destination = "", 5, "", ""
+        # Variables initialisation
+	self.flags = [False, False, False, False, False] # Time, Text, Duration, Encoding, Save 
+	self.supportEncoding = ['base64', 'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'all']
+        self.time, self.text, self.duration, self.encoding, self.destination = [], [], 0, [], ""
+
+        #Parser initialisation
         self.parser = argparse.ArgumentParser(description='Basic session generator')
 
-	self.parser.add_argument('-t', action="store", dest="time", help="Start time, format -> d/m/y h:min:s If the time is not specify the current time is taken")
-	self.parser.add_argument('-d', action="store", dest="duration", help="Duration after the start time in minutes By default : 5 mins", type=int)
-	self.parser.add_argument('-e', action="store", dest="encoding", help="Enconding wished. Currently support : 'md5', 'base64', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'. By default : None")
-	self.parser.add_argument('-s', action="store", dest="destination", help="File used to save the output")
+        self.parser.add_argument('-T', action="store", dest="time", help="Time(s) to encode, format -> d/m/y h:min:s Or type 'current' to use the current time", nargs='+')
+        self.parser.add_argument('-t', action="store", dest="text", help="Text(s) to encode", nargs='+')
+        self.parser.add_argument('-d', action="store", dest="duration", help="Duration after the start time in minutes.", type=int, nargs=1)
+        self.parser.add_argument('-e', action="store", dest="encoding", help="Enconding wished. Currently support : 'base64', 'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'. Type 'all' to use them all this order. By default : None", nargs='+')
+        self.parser.add_argument('-s', action="store", dest="destination", help="File used to save the output", nargs=1)
 
-	args, unknown = self.parser.parse_known_args()
+        if len(sys.argv)==1:
+            self.parser.print_help()
+            sys.exit(1)
+
+        args, unknown = self.parser.parse_known_args()
 
         # Check the arguments
-	if unknown:
-	    print 'Unknown argument(s) : ' + str(unknown).strip('[]') + '\n'
-	    self.parser.print_help()
-	    sys.exit()
-	
-	if args.time != None:
-	    self.time = args.time
-	if args.duration != None:
-	    self.duration = args.duration
-	if args.destination != None:
-	    self.destination = args.destination
-
-	if self.duration < 1:
-            print 'Duration must be at least 1 min\n'
-            self.parser.print_help()
-            sys.exit()
-
-	self.encoding = args.encoding
-	
-	self.main()
+        if unknown:
+            self.displayError('Unknown argument(s) : ' + str(unknown).strip('[]') + '\n')
+        
+	# Set the flags
+        if args.time != None:
+            self.time = list(args.time)
+	    self.flags[0] = True
+	if args.text !=None:
+	    self.text = list(args.text)
+	    self.flags[1] = True
+        if args.duration != None:
+            self.duration = args.duration[0]
+	    self.flags[2] = True
+        if args.encoding != None:
+            self.encoding = list(args.encoding)
+	    self.flags[3] = True
+        if args.destination != None:
+            self.destination = args.destination
+	    self.flags[4] = True
+        
+        self.main()
 
     def main(self):
-	if self.time == "":
-	    self.setTime()
+	"Main function"
+	timeList_start = [] # List of timestamp (from the input)
+	timeList_duration = [] # List of timestamp (from the input with duration)
+	textList_hash = [] # List of hash
 
-	listGen = self.generator()
-	if self.destination == "":
-	    print listGen
-	else:
-	    self.saveOutput(listGen)
+	# Date
+	if self.flags[0]:
+	    # Remplace 'currrent' by a formatted date
+            self.processTime()
+	    # Get the timestamp for each date
+	    for time in self.time:
+		timeList_start.append(self.getTimeInt(time))
 
-    def generator(self):
-	try:
-	    timestamp = int(time.mktime(datetime.datetime.strptime(self.time, "%d/%m/%Y %H:%M:%S").timetuple()))
-	except :
-	    print 'The time format is not valid\n'
-	    self.parser.print_help()
-	    sys.exit()
+	    if self.flags[2]: # Duration
+		if self.duration < 1:
+		    self.displayError('The duration must be at least 1 minute\n')
+		for time in timeList_start:
+		    timeList_duration.append(self.getTimeDuration(time))
+
+	# Texte without encoding
+	elif self.flags[1] and not self.flags[3]:
+	    self.displayError("Please select an encoding or type '-e all'\n")
+
+	# Duration without time
+	elif self.flags[2]:
+	    displayError("You need to type a datetime. Type '-T current' to use the current time.\n")
+
+	# Text without time
+	elif self.flags[1] and not self.flags[0]:
+	    if len(self.encoding) != 1 and len(self.encoding) != len(self.text):
+		if len(self.text) != 1:
+		    self.displayError('You must choose one encoding for all strings or one encoding for each string\n')
+	    if len(self.encoding) == 1: 
+	        for text in self.text:
+		    textList_hash.append(self.encoder(text, self.encoding[0]))
+	    else:
+		if len(self.text) != 1: # len(text) == len(encoding)
+		    for index, text in enumerate(self.text):
+		        textList_hash.append(self.encoder(text, self.encoding[index]))
+		else: # len(text) == 1
+		    for index, code in enumerate(self.encoding):
+			textList_hash.append(self.encoder(self.text[0], code))
+
+	# Date with encoding and without text
+	if self.flags[0] and self.flags[3] and not self.flags[1]:
+	    if len(self.encoding) != 1 and len(self.encoding) != len(self.time):
+		if len(self.time) != 1:
+		    self.displayError('You must choose one encoding for all dates or one encoding for each date\n')
+	    if not self.flags[2]:
+	        if len(self.encoding) == 1: 
+	            for time in timeList_start:
+		        textList_hash.append(self.encoder(time, self.encoding[0]))
+	        else:
+		    if len(self.time) != 1: # len(time) == len(encoding)
+		        for index, time in enumerate(timeList_start):
+		            textList_hash.append(self.encoder(time, self.encoding[index]))
+		    else: # len(time) == 1
+		        for index, code in enumerate(self.encoding):
+			    textList_hash.append(self.encoder(timeList_start[0], code))
+	    else:
+		if len(self.encoding) == 1: 
+	            for timeList in timeList_duration:
+			for time in timeList:
+		            textList_hash.append(self.encoder(time, self.encoding[0]))
+	        else:
+		    if len(self.time) != 1: # len(time) == len(encoding)
+		        for index, timeList in enumerate(timeList_duration):
+			    for time in timeList:
+		                textList_hash.append(self.encoder(time, self.encoding[index]))
+		    else: # len(time) == 1
+			for time in timeList_duration[0]:
+			    for code in self.encoding:
+		                textList_hash.append(self.encoder(time, code))
+
+	print textList_hash
 	    
-	timeList, encodedList, i, duration = [], [], 0, self.duration*60
-	while i < duration:
-	    timestamp += 1
-	    timeList.append(timestamp)
-	    i += 1
-
-	if self.encoding == None:
-	    return timeList
-
-	elif self.encoding == 'md5':
-	    for ts in timeList:
-		hashMd5 = hashlib.md5(str(ts).encode())
-		encodedList.append(hashMd5.hexdigest())
 	
-	elif self.encoding == 'base64':
-	    for ts in timeList:
-		encodedList.append(base64.b64encode(str(ts))[:-2])
+	
+        #listGen = self.encoder()
+        
+	# Save or display the output
+	#if self.flags[4]:
+            #self.saveOutput(listGen)
+        #else:
+	    #for gen in listGen:
+                #print gen	
 
-	elif self.encoding == 'sha1':
-	    for ts in timeList:
-		hashSha1 = hashlib.sha1(str(ts).encode())
-		encodedList.append(hashSha1.hexdigest())
+    def encoder(self, text, method):           
+        encodedList = []
 
-	elif self.encoding == 'sha224':
-	    for ts in timeList:
-		hashSha224 = hashlib.sha224(str(ts).encode())
-		encodedList.append(hashSha224.hexdigest())
+        if method == 'base64' or method == 'all':
+	    encodedList.append(base64.b64encode(str(text))[:-2])
 
-	elif self.encoding == 'sha256':
-	    for ts in timeList:
-		hashSha256 = hashlib.sha256(str(ts).encode())
-		encodedList.append(hashSha256.hexdigest())
+        if method == 'md5' or method == 'all':
+            hashMd5 = hashlib.md5(str(text).encode())
+            encodedList.append(hashMd5.hexdigest())
 
-	elif self.encoding == 'sha384':
-	    for ts in timeList:
-		hashSha384 = hashlib.sha384(str(ts).encode())
-		encodedList.append(hashSha384.hexdigest())
+        if method == 'sha1' or method == 'all':
+            hashSha1 = hashlib.sha1(str(text).encode())
+            encodedList.append(hashSha1.hexdigest())
 
-	elif self.encoding == 'sha512':
-	    for ts in timeList:
-		hashSha512 = hashlib.sha512(str(ts).encode())
-		encodedList.append(hashSha512.hexdigest())		
+        if method == 'sha224' or method == 'all':
+            hashSha224 = hashlib.sha224(str(text).encode())
+            encodedList.append(hashSha224.hexdigest())
 
-	else:
-	    print 'Wrong encoding\n'
-	    print self.parser.print_help()
-	    sys.exit()
+        if method == 'sha256'or method == 'all':
+            hashSha256 = hashlib.sha256(str(text).encode())
+            encodedList.append(hashSha256.hexdigest())
 
-	return encodedList
+        if method == 'sha384' or method == 'all':
+            hashSha384 = hashlib.sha384(str(text).encode())
+            encodedList.append(hashSha384.hexdigest())
 
-    def setTime(self):
-	now = datetime.datetime.now()
-	self.time = str(now.day) + '/' + str(now.month) + '/' + str(now.year) + ' ' + str(now.hour) + ':' + str(now.minute) + ':' + str(now.second)
+        if 'sha512' in self.encoding or 'all' in self.encoding:
+            hashSha512 = hashlib.sha512(str(text).encode())
+            encodedList.append(hashSha512.hexdigest())      
+
+        if not method in self.supportEncoding:
+            self.displayError('Wrong encoding\n')
+
+        return encodedList
+
+    def processTime(self):
+	"Remplace 'currrent' by a formatted date"
+	while 'current' in self.time:
+	    index = self.time.index("current")
+	    self.time[index] = self.getCurrentTimeString()
+
+    def getCurrentTimeString(self):
+	return time.strftime("%d/%m/%Y %H:%M:%S")
+
+    def getTimeInt(self, formattedTime):
+	try:
+	    return int(time.mktime(datetime.datetime.strptime(formattedTime, "%d/%m/%Y %H:%M:%S").timetuple())) 
+        except :
+            self.displayError('The time format is not valid\n')
+
+    def getTimeDuration(self, timestamp):
+	timeList, duration, i = [], self.duration*60, 0
+	while i < duration:
+	    timeList.append(timestamp)
+	    timestamp += 1
+	    i += 1
+	return timeList    
 
     def saveOutput(self, listGen):
-	myFile = open(self.destination, "w")
-	for gen in listGen:
-	    myFile.write(str(gen))
-	    myFile.write('\n')
-	myFile.close()
+        myFile = open(self.destination, "w")
+        for gen in listGen:
+            myFile.write(str(gen))
+            myFile.write('\n')
+        myFile.close()
+
+    def displayError(self, error):
+	print error
+	self.parser.print_help()
+	sys.exit()	
 
 # Programme de test
 if __name__ == "__main__":
