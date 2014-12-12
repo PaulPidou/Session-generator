3d523d1f668915ea28317b348edb9496fb0ab825
PaulPidou <pidou.paul@gmail.com>
2014-12-12 11:40:42 +0100

    Remove concatenation
    Will be in another script

--- session_generator.py
+++ session_generator.py
@@ -8,9 +8,9 @@ class Application():
     "Basic session generator"
     def __init__(self):
         # Variables initialisation
-	self.flags = [False, False, False, False, False, False, False] # Time, Text, Duration, Encoding, Concatenate, Order, Save 
+	self.flags = [False, False, False, False, False, False] # Time, Text, Duration, Cooie-friendly, Encoding, Save 
 	self.supportEncoding = ['base64', 'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'all']
-        self.time, self.text, self.duration, self.encoding, self.order, self.destination = [], [], 0, [], [], ""
+        self.time, self.text, self.duration, self.encoding, self.destination = [], [], 0, [], ""
 
         #Parser initialisation
         self.parser = argparse.ArgumentParser(description='Basic session generator')
@@ -19,8 +19,7 @@ class Application():
         self.parser.add_argument('-t', action="store", dest="text", help="Text(s) to encode", nargs='+')
         self.parser.add_argument('-d', action="store", dest="duration", help="Duration after the start time in minutes.", type=int, nargs=1)
         self.parser.add_argument('-e', action="store", dest="encoding", help="Enconding wished. Currently support : 'base64', 'md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'. Type 'all' to use them all this order. By default : None", nargs='+')
-        self.parser.add_argument('-c', action="store_true", default=False, dest="concatenate", help="Set the contenation to true.") 
-        self.parser.add_argument('-o', action='store', dest="order", help="Set the order for the concatenation when you use time and text together. Type 'text' and 'time' in the order you want to concatenate them.", nargs="+")
+        self.parser.add_argument('-cf', action="store_true", default=False, dest="cookie_friendly", help="The output will be cookie friendly.") 
         self.parser.add_argument('-s', action="store", dest="destination", help="File to save the output", nargs=1)
 
         if len(sys.argv)==1:
@@ -46,14 +45,11 @@ class Application():
         if args.encoding != None:
             self.encoding = list(args.encoding)
 	    self.flags[3] = True
-	if args.order != None:
-            self.order = list(args.order)
-            self.flags[5] = True
         if args.destination != None:
-            self.destination = args.destination
-	    self.flags[6] = True
+            self.destination = args.destination[0]
+	    self.flags[5] = True
 
-	self.flags[4] = args.concatenate
+	self.flags[4] = args.cookie_friendly
         
         self.main()
 
@@ -77,6 +73,16 @@ class Application():
 		for time in timeList_start:
 		    timeList_duration.append(self.getTimeDuration(time))
 
+	    if self.flags[3]: # Encoding
+                if timeList_duration:
+                    for time in timeList_duration:
+                        for code in self.encoding:
+                            textList_hash.append(self.encoder(time, code))
+                else:
+                    for time in timeList_start:
+                        for code in self.encoding:
+                            textList_hash.append(self.encoder(time, code))
+
 	# Text without encoding
 	elif self.flags[1] and not self.flags[3]:
 	    self.displayError("Please select an encoding or type '-e all'\n")
@@ -85,88 +91,28 @@ class Application():
 	elif self.flags[2]:
 	    self.displayError("You need to type a datetime. Type '-T current' to use the current time.\n")
 
-        # Check the contenation 
-	if self.flags[5] and not self.flags[4]:
-            self.displayError("You have to activate the concatenation before setting an order.\n")
-
-        if self.flags[5] and not self.flags[0] or not self.flags[1]:
-            self.displayError("You must use text and time together to set an order.\n")
-
-        if self.flags[5]:
-            if len(self.order) != self.order.count('time') + self.order.count('text'):
-                self.displayError("Type only 'time' and 'text' to set the order.\n")
-            elif len(self.order) != len(self.text) + len(self.time):
-                self.displayError("You have to type as many position chains as input.\n")
-            elif len(self.text) != self.order.count('text') or len(self.time) != self.order.count('time'):
-                self.displayError("You have to type as many 'text' in the order as text input and as many 'time' in the order as time input.\n")
-
-	# Text without time
-	elif self.flags[1] and not self.flags[0]:
-	    if len(self.encoding) != 1 and len(self.encoding) != len(self.text):
-		if len(self.text) != 1:
-		    self.displayError('You must choose one encoding for all strings or one encoding for each string\n')
-	    if len(self.encoding) == 1: 
-	        for text in self.text:
-		    textList_hash.append(self.encoder(text, self.encoding[0]))
-	    else:
-		if len(self.text) != 1: # len(text) == len(encoding)
-		    for index, text in enumerate(self.text):
-		        textList_hash.append(self.encoder(text, self.encoding[index]))
-		else: # len(text) == 1
-		    for index, code in enumerate(self.encoding):
-			textList_hash.append(self.encoder(self.text[0], code))
-
-	# Date with encoding and without text
-	if self.flags[0] and self.flags[3] and not self.flags[1]:
-	    if len(self.encoding) != 1 and len(self.encoding) != len(self.time):
-		if len(self.time) != 1:
-		    self.displayError('You must choose one encoding for all dates or one encoding for each date\n')
-	    if not self.flags[2]:
-	        if len(self.encoding) == 1: 
-	            for time in timeList_start:
-		        textList_hash.append(self.encoder(time, self.encoding[0]))
-	        else:
-		    if len(self.time) != 1: # len(time) == len(encoding)
-		        for index, time in enumerate(timeList_start):
-		            textList_hash.append(self.encoder(time, self.encoding[index]))
-		    else: # len(time) == 1
-		        for index, code in enumerate(self.encoding):
-			    textList_hash.append(self.encoder(timeList_start[0], code))
-	    else:
-		if len(self.encoding) == 1: 
-	            for timeList in timeList_duration:
-			for time in timeList:
-		            textList_hash.append(self.encoder(time, self.encoding[0]))
-	        else:
-		    if len(self.time) != 1: # len(time) == len(encoding)
-		        for index, timeList in enumerate(timeList_duration):
-			    for time in timeList:
-		                textList_hash.append(self.encoder(time, self.encoding[index]))
-		    else: # len(time) == 1
-			for time in timeList_duration[0]:
-			    for code in self.encoding:
-		                textList_hash.append(self.encoder(time, code))
-
-        # Date with text
-
-	print textList_hash
-	    
-	
-	
-        #listGen = self.encoder()
+	# Text
+	if self.flags[1]:
+            for text in self.text:
+                for code in self.encoding:
+                    textList_hash.append(self.encoder(text, code))
         
 	# Save or display the output
-	#if self.flags[4]:
-            #self.saveOutput(listGen)
-        #else:
-	    #for gen in listGen:
-                #print gen	
+	if self.flags[5]:
+            self.saveOutput(textList_hash)
+        else:
+	    for textList in textList_hash:
+                for text in textList:
+                    print text	
 
     def encoder(self, text, method):           
         encodedList = []
 
         if method == 'base64' or method == 'all':
-	    encodedList.append(base64.b64encode(str(text))[:-2])
+            if self.flags[4]: # Cookie friendly
+                encodedList.append(base64.b64encode(str(text)).strip('='))
+            else:
+                encodedList.append(base64.b64encode(str(text)))
 
         if method == 'md5' or method == 'all':
             hashMd5 = hashlib.md5(str(text).encode())
@@ -220,11 +166,12 @@ class Application():
 	    i += 1
 	return timeList    
 
-    def saveOutput(self, listGen):
+    def saveOutput(self, textList_hash):
         myFile = open(self.destination, "w")
-        for gen in listGen:
-            myFile.write(str(gen))
-            myFile.write('\n')
+        for textList in textList_hash:
+            for text in textList:
+                myFile.write(str(text))
+                myFile.write('\n')
         myFile.close()
 
     def displayError(self, error):
